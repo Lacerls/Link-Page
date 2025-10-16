113e7ca2b63047b959917fddb3afef891122eee5
Leonardo Silva <leonardo.silva.lacerda23@gmail.com>
2025-10-16 16:54:17 -0300

    Update script.js

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function github() {
+    window.location.href = "https://github.com/Lacerls";
+}
+
+function github() {
+    window.location.href = "https://github.com/Lacerls";
+}
+
+function instagram() {
+    window.location.href = "https://instagram.com/Lacerls_";
+}
